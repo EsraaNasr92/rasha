88808f9324965b12da01ea273c49f4494d6e834d
EsraaNasr92 <esraanasr706@gmail.com>
2022-08-01 11:25:00 +0200

    about page

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Page;
 
 class AboutController extends Controller
 {
-    public function index(){                
-        return view('about.index');
+    public function index(){ 
+        $page = Page::where('slug', '=', 'about')->get('content');
+        return view('home.about', ['page' => $page]);
     }
 }
